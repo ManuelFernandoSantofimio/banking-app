f66130c2a212bdfd69b2d70d66dce919afe22ef5
ManuelFernandoSantofimio <msantofimio@serempre.com>
2023-03-15 14:59:32 -0500

    Tabledb

--- banking-app/src/main/java/com/bankingapp/bankingapp/domain/entity/Colchon.java
+++ banking-app/src/main/java/com/bankingapp/bankingapp/domain/entity/Colchon.java
@@ -9,10 +9,41 @@ import java.math.BigDecimal;
 public class Colchon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
+    @Column(name = "id_colchon", nullable = false, unique = true)
     private Long id;
     @Column(name = "name", length = 255)
     private String name;
     @Column(name = "saldo", length = 255)
     private BigDecimal saldo;
+    @ManyToOne
+    private User user;
+
+    public Colchon() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Colchon{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", saldo=" + saldo +
+                ", user=" + user +
+                '}';
+    }
 }
--- banking-app/src/main/java/com/bankingapp/bankingapp/domain/entity/Cuenta.java
+++ banking-app/src/main/java/com/bankingapp/bankingapp/domain/entity/Cuenta.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class Cuenta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, unique = true)
+    @Column(name = "id_cuenta", nullable = false, unique = true)
     private Long id;
     @Column(name = "name", length = 255)
     private String name;
@@ -16,5 +16,44 @@ public class Cuenta {
     private String email;
     @Column(name = "pass", length = 255)
     private String pass;
+    @ManyToOne
+    private User user;
 
+    public Cuenta() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", pass='" + pass + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
